d6abce477df0776c017a2ee977afaa17ec8e2584
Gowtham-5798 <122111618+Gowtham-5798@users.noreply.github.com>
2023-11-17 10:59:30 +0530

    Update providers.tf

--- EKS & Help Deployment Terraform code/providers.tf
+++ EKS & Help Deployment Terraform code/providers.tf
@@ -4,13 +4,13 @@ provider "aws" {
 
 provider "helm" {
   kubernetes {
-    config_path = "C:\\Users\\gowth\\.kube\\config"
+    config_path = "C:\\Users\\user\\.kube\\config"
   }
 }
 
 terraform {
   backend "s3" {
-    bucket  = "terraform-stovl"
+    bucket  = "terraform-example"
     key     = "my-eks-terraform-project/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
