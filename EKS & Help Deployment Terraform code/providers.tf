56f1644c3987356dca654c1e2bcb080ad1da9888
Gowtham-5798 <122111618+Gowtham-5798@users.noreply.github.com>
2023-11-20 15:55:56 +0530

    Update providers.tf

--- EKS & Help Deployment Terraform code/providers.tf
+++ EKS & Help Deployment Terraform code/providers.tf
@@ -2,10 +2,44 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# You can either follow way-1 or way-2 to deploy the helm
+# way-1
 provider "helm" {
   kubernetes {
     config_path = "C:\\Users\\user\\.kube\\config"
   }
+}
+
+# way-2
+data "aws_eks_cluster" "eks_cluster" {
+  depends_on = [module.eks]
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "aws_cluster_auth" {
+  depends_on = [module.eks]
+  name = module.eks.cluster_name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.aws_cluster_auth.token
+  }
+}
+
+output "eks_cluster_endpoint" {
+  value = data.aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "eks_cluster_ca_certificate" {
+  value = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
+}
+
+output "eks_cluster_token" {
+  value = data.aws_eks_cluster_auth.aws_cluster_auth.token
+  sensitive = true
 }
 
 terraform {
